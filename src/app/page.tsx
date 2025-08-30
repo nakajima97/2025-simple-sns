9715d6b9c050fb3560f3cd3cb68ab8fbb56bf99c
nakajima97 <kituneudon97@gmail.com>
2025-08-30 14:12:57 +0900

    #1 tailwind cssのインストール

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <p>Hello, world!</p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </main>
   );
 }
