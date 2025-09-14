adeeb34d1a4a738d6ba90942f90aedf6dcad590d
nakajima97 <kituneudon97@gmail.com>
2025-09-14 15:10:55 +0900

    #5 関数化

--- src/db/client.ts
+++ src/db/client.ts
@@ -2,10 +2,14 @@ import 'dotenv/config';
 import '@/lib/envConfig'
 import { drizzle } from 'drizzle-orm/libsql';
 
-const fileName = process.env.DB_FILE_NAME;
+export const getClient = () => {
+  const fileName = process.env.DB_FILE_NAME;
 
-if (!fileName) {
-  throw new Error("SQLiteのファイル名が環境変数で設定されていないです。");
+  if (!fileName) {
+    throw new Error("SQLiteのファイル名が環境変数で設定されていないです。");
+  }
+
+  const client = drizzle(fileName);
+
+  return client
 }
-
-export const client = drizzle(fileName);
