ea5d04ad96f39ba6cdbd32f67a16107ff422f8ce
nakajima97 <kituneudon97@gmail.com>
2025-09-20 14:56:57 +0900

    #8 ログインページを作成

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,47 @@
-const Page = () => {
-  return <h1>Hello Next.js!</h1>
-}
+import { Button } from "@/components/ui/shadcn/button";
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/shadcn/card";
+import { Input } from "@/components/ui/shadcn/input";
+import { Label } from "@/components/ui/shadcn/label";
+
+const Page = () => (
+  <div className="flex justify-center items-center w-full h-dvh">
+    <Card className="w-full max-w-sm h-fit">
+      <CardHeader>
+        <CardTitle>Login to your account</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+        <CardAction>
+          <Button variant="link">Sign Up</Button>
+        </CardAction>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required />
+            </div>
+            <div className="grid gap-2">
+              <Input id="password" type="password" required />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+        <Button variant="outline" className="w-full">
+          Login with Google
+        </Button>
+      </CardFooter>
+    </Card>
+  </div>
+)
 
 export default Page;
